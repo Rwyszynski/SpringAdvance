314569f7b47cb481499e4053e4596160e94ee571
Robert Wyszynski <robertointerwento@gmail.com>
2024-12-27 18:41:03 +0100

    2.1

---
+++ src/test/java/com/kodilla/strefakursow/StrefaKursowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.strefakursow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrefaKursowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
